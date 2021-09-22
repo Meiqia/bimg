61050a69e0c1406f89567d8a76ad77fcdbf08eab
dinglin <d1nglin@qq.com>
2021-09-22 15:13:42 +0800

    适配美洽环境

--- options.go
+++ options.go
@@ -1,7 +1,7 @@
 package bimg
 
 /*
-#cgo CFLAGS: -I. -I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include -I/usr/include/libpng16 -I/usr/include -I/usr/include/png
+#cgo CFLAGS: -I. -I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include -I/usr/include/libpng16 -I/usr/include -I/usr/include/png -I/usr/lib64/glib-2.0/include
 #cgo LDFLAGS: -lvips -lgobject-2.0 -lglib-2.0
 #include "vips/vips.h"
 */
